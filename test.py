ffb24aefd2430e56d02a7444cb56a39977cc5d68
Johnny19Yuan <johnny19yuan@gmail.com>
2025-09-08 09:24:36 -0400

    turtl

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+equal(200)
+
+turtle(done)
